d6d07660f7eae9fb92f82764d58a0ab94c4c84e9
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-08-09 22:12:30 +0700

    Update reyette.h

--- src/reyette.h
+++ src/reyette.h
@@ -8,6 +8,7 @@ void centerText(const char* text, int y, int size=1);
 void typewriterText(const char* text, int y, int size, int charDelay);
 void scrambleToText(const char* target, int y, int size, int stepDelay);
 void scrambleTypewriterText(const char* target, int y, int size, int charDelay);
+void errorScreenEffect();
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64 // changes your screen size
