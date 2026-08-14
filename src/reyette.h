7cca40a2205c23e9ee2d018bf11b0677391059ce
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-08-14 17:06:16 +0700

    Update reyette.h

--- src/reyette.h
+++ src/reyette.h
@@ -4,8 +4,20 @@
 extern Adafruit_SSD1306 display;
 
 void initDisplay();
+void showLogo();
 void centerText(const char* text, int y, int size=1);
 void typewriterText(const char* text, int y, int size, int charDelay);
 void scrambleToText(const char* target, int y, int size, int stepDelay);
 void scrambleTypewriterText(const char* target, int y, int size, int charDelay);
 void errorScreenEffect();
+
+#if defined(LCD32)
+  #define SCREEN_HEIGHT 32
+#elif defined(LCD64)
+  #define SCREEN_HEIGHT 64
+#else
+  #error "Use LCD32 or LCD64"
+#endif
+
+#define SCREEN_WIDTH 128
+#define OLED_BIG (SCREEN_HEIGHT == 64)
